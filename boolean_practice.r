a81170e408387952d365f78777ea57604e6b6bff
Ashle3 <ashleeb3@byui.edu>
2023-06-17 23:11:52 -0500

    finished boolean practice

---
+++ boolean_practice.r
@@ -0,0 +1,27 @@
+#this file will let me practice the boolean data type
+
+#condition where x is less than y
+x <- 1000
+y <- 10000
+
+print(x > y) #FALSE
+print(x < y) #TRUE
+print(x == y) #FALSE
+print("=============")
+
+#condition where x is greater than y
+x1 <- 25
+y1 <- 20
+
+print(x1 > y1) #TRUE
+print(x1 < y1)  #FALSE
+print(x1 == y1) #FALSE
+print("=============")
+
+#condition where x and y are equal
+x2 <- 100
+y2 <- 100
+
+print(x2 > y2) #FALSE
+print(x2 < y2)  #FALSE
+print(x2 == y2) #TRUE
